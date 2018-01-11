82b715803481cfa7a35d18edb9346d70eb354e1c
Andrew Friedman <andrewfriedman01@gmail.com>
2018-01-10 22:32:24 -0500

    Added App
    This is the first version of Fake Coin Flip that GitHub has ever seen!

---
+++ Assets/MainMenu.js
@@ -0,0 +1,116 @@
+﻿#pragma strict
+var Press : GameObject; //Beginning thing
+
+var DollarH : GameObject;
+var DollarT : GameObject;
+var QuarterH : GameObject;
+var QuarterT : GameObject;
+var DimeH : GameObject;
+var DimeT : GameObject;
+var NickelH : GameObject;
+var NickelT : GameObject;
+var PennyH : GameObject;
+var PennyT : GameObject;
+
+
+function Start () {
+	if (PlayerPrefs.GetInt("Intro") == 0) {
+		Press.SetActive (true);
+		PlayerPrefs.SetString("Coin", "Select Coin");
+		PlayerPrefs.SetString("Trick", "Rip");
+		QuarterH.SetActive (true);
+	}else {
+		if (PlayerPrefs.GetString("Coin") == "DollarH" || PlayerPrefs.GetString("Coin") == "DollarT") {
+			DollarH.SetActive (true);
+		}else if (PlayerPrefs.GetString("Coin") == "QuarterH" || PlayerPrefs.GetString("Coin") == "QuarterT") {
+			QuarterH.SetActive (true);
+		}else if (PlayerPrefs.GetString("Coin") == "DimeH" || PlayerPrefs.GetString("Coin") == "DimeT") {
+			DimeH.SetActive (true);
+		}else if (PlayerPrefs.GetString("Coin") == "NickelH" || PlayerPrefs.GetString("Coin") == "NickelT") {
+			NickelH.SetActive (true);
+		}else if (PlayerPrefs.GetString("Coin") == "PennyH" || PlayerPrefs.GetString("Coin") == "PennyT") {
+			PennyH.SetActive (true);
+		}else {
+			Debug.Log("Error loading Coin");
+		}
+	}
+}
+
+function Update () {
+
+}
+
+function Flip () {
+	var number = Random.Range(3,10);
+	if (PlayerPrefs.GetString("Trick") == "Lmao" && number == 4 || number == 6 || number == 8 || number == 10) {
+		number++;
+	}
+	//Debug.Log (number);
+	while (number != 0) {
+		Debug.Log(number);
+		if (PlayerPrefs.GetString("Coin") == "DollarH") {
+			Killit();
+			DollarT.SetActive (true);
+			PlayerPrefs.SetString("Coin", "DollarT");
+		}else if (PlayerPrefs.GetString("Coin") == "DollarT") {
+			Killit();
+			DollarH.SetActive (true);
+			PlayerPrefs.SetString("Coin", "DollarH");
+		}else if (PlayerPrefs.GetString("Coin") == "QuarterH") {
+			Killit();
+			QuarterT.SetActive (true);
+			PlayerPrefs.SetString("Coin", "QuarterT");
+		}else if (PlayerPrefs.GetString("Coin") == "QuarterT") {
+			Killit();
+			QuarterH.SetActive (true);
+			PlayerPrefs.SetString("Coin", "QuarterH");
+		}else if (PlayerPrefs.GetString("Coin") == "DimeH") {
+			Killit();
+			DimeT.SetActive (true);
+			PlayerPrefs.SetString("Coin", "DimeT");
+		}else if (PlayerPrefs.GetString("Coin") == "DimeT") {
+			Killit();
+			DimeH.SetActive (true);
+			PlayerPrefs.SetString("Coin", "DimeH");
+		}else if (PlayerPrefs.GetString("Coin") == "NickelH") {
+			Killit();
+			NickelT.SetActive (true);
+			PlayerPrefs.SetString("Coin", "NickelT");
+		}else if (PlayerPrefs.GetString("Coin") == "NickelT") {
+			Killit();
+			NickelH.SetActive (true);
+			PlayerPrefs.SetString("Coin", "NickelH");
+		}else if (PlayerPrefs.GetString("Coin") == "PennyH") {
+			Killit();
+			PennyT.SetActive (true);
+			PlayerPrefs.SetString("Coin", "PennyT");
+		}else if (PlayerPrefs.GetString("Coin") == "PennyT") {
+			Killit();
+			PennyH.SetActive (true);
+			PlayerPrefs.SetString("Coin", "PennyH");
+		}
+		number--;
+	}
+	PlayerPrefs.SetString("Trick", "Rip");
+}
+
+function numburo () {
+	
+}
+function Killit () {
+	DollarT.SetActive (false);
+	DollarH.SetActive (false);
+	QuarterT.SetActive (false);
+	QuarterH.SetActive (false);
+	DimeT.SetActive (false);
+	DimeH.SetActive (false);
+	NickelT.SetActive (false);
+	NickelH.SetActive (false);
+	PennyT.SetActive (false);
+	PennyH.SetActive (false);
+}
+
+function settings () {
+	PlayerPrefs.SetInt("Intro", 1);
+	Application.LoadLevel("Settings");
+}
---
+++ Assets/Settings.js
@@ -0,0 +1,96 @@
+﻿#pragma strict
+var Dropdown : UnityEngine.UI.Text;
+var Checker : UnityEngine.UI.Toggle;
+
+var Dollar : GameObject;
+var Quarter : GameObject;
+var Dime : GameObject;
+var Nickel : GameObject;
+var Penny : GameObject;
+
+var stop : System.Boolean;
+
+
+function Start () {
+	if (PlayerPrefs.GetString("Trick") == "Lmao") {
+		stop = true;
+		Checker.isOn = true;
+		stop = false;
+	}
+}
+
+function Update () {
+	if (PlayerPrefs.GetString("Coin") == "DollarH" || PlayerPrefs.GetString("Coin") == "DollarT") {
+		Dropdown.text = "Dollar Coin";
+	}else if (PlayerPrefs.GetString("Coin") == "QuarterH" || PlayerPrefs.GetString("Coin") == "QuarterT") {
+		Dropdown.text = "Quarter";
+	}else if (PlayerPrefs.GetString("Coin") == "DimeH" || PlayerPrefs.GetString("Coin") == "DimeT") {
+		Dropdown.text = "Dime";
+	}else if (PlayerPrefs.GetString("Coin") == "NickelH" || PlayerPrefs.GetString("Coin") == "NickelT") {
+		Dropdown.text = "Nickel";
+	}else if (PlayerPrefs.GetString("Coin") == "PennyH" || PlayerPrefs.GetString("Coin") == "PennyT") {
+		Dropdown.text = "Penny";
+	}
+}
+
+function SELECTORZ ()  {
+	if (Dollar.active == true) {
+		Dollar.SetActive (false);
+		Quarter.SetActive (false);
+		Dime.SetActive (false);
+		Nickel.SetActive (false);
+		Penny.SetActive (false);
+	}else if (Dollar.active == false) {
+		Dollar.SetActive (true);
+		Quarter.SetActive (true);
+		Dime.SetActive (true);
+		Nickel.SetActive (true);
+		Penny.SetActive (true);
+	}
+}
+
+function Prankster () {
+	if (stop == true) {
+	
+	}else if (stop == false) {
+		if (PlayerPrefs.GetString("Trick") == "Lmao") {
+			PlayerPrefs.SetString("Trick", "Rip");
+			Debug.Log("Not a prank bro");
+		}else if (PlayerPrefs.GetString("Trick") == "Rip") {
+			PlayerPrefs.SetString("Trick", "Lmao");
+			Debug.Log("Just a prank bro");
+		}else {
+			PlayerPrefs.SetString("Trick", "Lmao");
+			Debug.Log("ERROR PLAYPREF WASNT SET");
+		}
+	}
+}
+
+function DollarSETTER () {
+	PlayerPrefs.SetString("Coin", "DollarH");
+	SELECTORZ();
+}
+
+function QuarterSETTER () {
+	PlayerPrefs.SetString("Coin", "QuarterH");
+	SELECTORZ();
+}
+
+function DimeSETTER () {
+	PlayerPrefs.SetString("Coin", "DimeH");
+	SELECTORZ();
+}
+
+function NickelSETTER () {
+	PlayerPrefs.SetString("Coin", "NickelH");
+	SELECTORZ();
+}
+
+function PennySETTER () {
+	PlayerPrefs.SetString("Coin", "PennyH");
+	SELECTORZ();
+}
+
+function BACK () {
+	Application.LoadLevel("MainMenu");
+}
